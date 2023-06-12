2f8f115f986547173c02df811f78197006500996
Cyberguru1 <hamzacypher34@gmail.com>
2023-06-12 11:10:15 -0500

    more writeup

--- Arrays/main.go
+++ Arrays/main.go
@@ -29,17 +29,46 @@ func main() {
 	}
 
 	// using classic for loops
-	fmt.Println("\nUising classical for loops to print content of ar3\n")
+	fmt.Println("\nUsing classical for loops to print content of ar3\n")
 	for i:=0; i<len(ar3); i++ {
 		fmt.Println("index :", i, "value :", ar3[i])
 	}
 
 	// changing the values of an array
 
-	new_arr := [...]int{}
-
-
-
-
+	new_arr := [5]int{}
+	fmt.Printf("\nCurrent array content:%#v\n", new_arr)
 	
+	fmt.Println("changing the array content:\n")
+
+	for index, _ := range new_arr {
+		new_arr[index] = (index * 2) + 1
+	}
+
+	fmt.Printf("After changing : %#v\n", new_arr)
+
+	// Declaring an array with new lines
+
+	new_arr2 := [...]int{
+		1,
+		4,
+		5,
+		6,
+		7, // must always end with coma
+	}
+
+	fmt.Printf("\n%#v\n", new_arr2)
+	
+	// keyed elements, simillar to dictionary
+
+	key_arr := [...]int{
+		1 : 5,
+		0 : 44,
+		2 : 55,
+		4 : 20,
+		3 : 12, // order doesen't matter, what matters for arrangement is the keys
+	}
+
+	fmt.Printf("\nKey Elements: %#v\n", key_arr)
+
 }
