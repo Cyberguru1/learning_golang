12fee2489dce0612e0c9c73a217660863ee8c1b8
Cyberguru1 <hamzacypher34@gmail.com>
2023-06-20 11:02:19 -0500

    more writeup

--- Structs/main.go
+++ Structs/main.go
@@ -5,3 +5,59 @@
 //////////////////////////////////////
 
 package main
+
+
+import (
+	"fmt"
+	"strings"
+)
+
+
+func main() {
+	p := fmt.Println
+	pf := fmt.Printf
+	_, _ = p, pf
+
+	// declaring structs
+
+	type contact struct {
+		name, email, address string
+		id float64
+	}
+
+	pf("%#v\n", strings.Repeat("%",4))
+
+	details := contact{
+		"hamza", 
+		"hamza@cyb3rguru.com", 
+		"127.0.0.1",
+		99.9,
+	}
+
+	p(details)
+	
+	//accessing items
+
+	pf("name : %#v\n", details.name)
+
+	// updating a value
+
+	details.name = "Shazam"
+
+	pf("name : %#v\n", details)
+
+	// initializing an anonymous stuct datatype
+
+	Employee := struct {
+		name, shape, size, height string
+	}{
+		name: "ujiro",
+		shape:	"##cm",
+		size: "###",
+		height: "##",
+		}
+
+	pf("%#v\n",Employee)
+
+
+}
