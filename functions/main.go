ece5ddef3f0541f6ebe53033dd9ef0a96c941421
Cyberguru1 <hamzacypher34@gmail.com>
2023-06-23 00:01:10 -0500

    more writeup

---
+++ functions/main.go
@@ -0,0 +1,15 @@
+///////////////////////////////////////
+//        !!Hamza Saidu  !!          //
+//        !!Cyb3rguru    !!          //
+//        !!files in Go  !!          //
+//////////////////////////////////////
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
