fca558b47369de01293777a30db5f2b9f585a8ab
Cyberguru1 <hamzacypher34@gmail.com>
2023-06-23 02:02:16 -0500

    more writeup

---
+++ pointers/main.go
@@ -0,0 +1,45 @@
+///////////////////////////////////////
+//        !!Hamza Saidu     !!       //
+//        !!Cyb3rguru       !!       //
+//        !!functions in Go !!       //
+//////////////////////////////////////
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// pointers in Go
+
+	name := "CyberHashira"
+	fmt.Println(&name) // prints address of name
+	fmt.Println()
+	// storing address
+
+	var x int
+
+	ptr := &x
+
+	fmt.Printf("ptr is type %T with value %v and address %p\n", ptr, ptr, &ptr)
+
+	// declaring a pointer
+
+	var ptr1 *float64
+	_ = ptr1
+
+	// creating a pointer using new
+
+	p := new(int)
+
+	x = 100
+
+	p = &x
+
+	fmt.Printf("p is of type %T, and content %v with value %v\n", p, *p, p)
+	fmt.Printf("address of x is %p\n", x)
+
+
+
+}
