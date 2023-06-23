386a123cb432251052cba3e983155be9c8bac92d
Cyberguru1 <hamzacypher34@gmail.com>
2023-06-23 01:19:46 -0500

    go function

--- functions/main.go
+++ functions/main.go
@@ -100,7 +100,7 @@ func cube(x float64) (result float64) {
 	return
 }
 
-func searchItem(item string, arr_str... string) (result bool) {
+func searchItem(item string, arr_str ...string) (result bool) {
 	fmt.Println(string)
 	return
 }
